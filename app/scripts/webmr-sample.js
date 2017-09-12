f6bcfb0c873f800bdb4568ed143c81eaf9c04f5e
moritanian <pickit2express@gmail.com>
2017-09-13 07:34:25 +0900

    optical  flow と orientation 融合

--- app/scripts/webmr-sample.js
+++ app/scripts/webmr-sample.js
@@ -7,25 +7,134 @@ $(function() {
     far: 3000000
   };
 
-  let camera, scene, renderer;
-  initScene(false);
+  let camera, scene, renderer, container;
+  let objectControl, targetObject, group;
+
+  // webVR Enter button
+  var webvrEnterButton = document.createElement("button");
+  webvrEnterButton.id = "vr-enter-button";
+  webvrEnterButton.classList.add("vr-enter-button");
+  webvrEnterButton.addEventListener("click", function(){
+    WebVRSetting.init(renderer, camera, scene, "./../images/controllers/", {
+      useStereoVR: true,
+      useHeadsetVR: false
+    });
+    WebMR.setFullScreen(container);
+  });
+
+  document.body.appendChild(webvrEnterButton);
+
+  // screen shot button
+  var takeButton = document.getElementById("take-button");
+  takeButton.addEventListener("click", function(){
+
+    screenShot();
+
+  });
+
+ function screenShot(){
+    var context = renderer.domElement.getContext("experimental-webgl", {preserveDrawingBuffer: true});
+    var url = renderer.domElement.toDataURL('image/png');
+    var d = new Date();
+    var u_time = Math.floor((d.getTime())/1000);
+    var filename = "webmr" + u_time + ".png";
+    download(url, filename);
+  };
+
+  function download(objectURL, filename) {
+    var a = document.createElement('a');
+    var e = document.createEvent('MouseEvent');
+    //console.log(objectURL);
+
+    //a要素のdownload属性にファイル名を設定
+    a.download = filename;
+    a.href = objectURL;
+
+    //clickイベントを着火
+    e.initEvent("click", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+  };
+
+  // reset button
+  var resetButton = document.getElementById("reset-button");
+  resetButton.addEventListener("click", function(){
+    WebMR.resetControls();
+  });
+
+  // character button
+  var characterButton = document.getElementById("character-button");
+  var characterDialog = document.getElementById("character-dialog");
+  characterButton.addEventListener("click", function(){
+
+    if(characterDialog.style.display == "block"){
+      characterDialog.style.display = "none";
+    } else {
+      characterDialog.style.display = "block";
+    }
+
+  });
+
+  document.addEventListener("click", function(){
+    //characterDialog.style.display = "none";
+  });
+
+  var addCharacterSelectDialog = function(){
+
+    var charactersContainer = document.getElementById("characters-container");
+
+    return function(model){
+
+      var characterElement = document.createElement("span");
+
+      characterElement.classList.add("character-element");
+
+      var imgElement = document.createElement("img");
+
+      imgElement.setAttribute("src", model.imageUrl);
+
+      imgElement.classList.add("character-img");
+
+      characterElement.appendChild(imgElement);
+
+      characterElement.addEventListener('click', function(){
+
+        characterDialog.style.display = "none";
+
+        console.log(targetObject);
+
+        targetObject.visible = false;
+
+        model.mesh.visible = true;
+
+        targetObject = model.mesh;
+
+      });
+
+      charactersContainer.appendChild(characterElement);
+
+    }
+
+  }();
 
   /**
   * Initialize the scene.
   * @param {bool} antialias exec antialias
   */
   function initScene(antialias){
-    let container = document.createElement( 'div' );
+    container = document.createElement( 'div' );
     container.style.position = "absolute";
     container.style.right = "0";
     container.style.left = "0";
     container.style.top = "0";
     container.style.bottom = "0";
+    container.style.overflow = "hidden";
     document.body.appendChild( container );
 
-    renderer = new THREE.WebGLRenderer({antialias: antialias === true});
+    renderer = new THREE.WebGLRenderer({antialias: antialias === true, preserveDrawingBuffer: true});
+    renderer.domElement.id = "canvas";
     renderer.domElement.style.position = "relative";
-    renderer.domElement.style.left = "-4px";
+
+    //renderer.domElement.style.left = "-4px";
 
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -42,7 +151,7 @@ $(function() {
         cameraParams.near,
         cameraParams.far );
 
-    camera.position.set(0, 0, 0);
+    camera.position.set(0, 0,  8.0);
     scene.add(camera);
 
     // light
@@ -51,7 +160,7 @@ $(function() {
     light.position.set( -1, 1, -1 );
     scene.add( light );
 
-    let group = new THREE.Group();
+    group = new THREE.Group();
     scene.add( group );
 
     var onProgress = function ( xhr ) {
@@ -66,34 +175,62 @@ $(function() {
 
     var xPos = -10.0;
 
-    var onLoad = function(object){
+    var onLoad = function(mesh, model){
 
-      var mesh = object;
-      mesh.position.y = -3.0;
+      // 回転中心をずらす
+      mesh.geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, - 10.0 , 0) );
+
+      mesh.position.y = 0.0;
       mesh.position.x = 0;
-      mesh.position.z = - 8.0;
+      mesh.position.z = 0.0; //- 8.0;
       mesh.scale.multiplyScalar(0.36);
+
+      //mesh.rotation.x = Math.PI * 0.2;
       //mesh.rotation.y = Math.PI * 0.5;
-      xPos += 10.0;
+      //xPos += 10.0;
 
       //makePhongMaterials ( mesh )
 
       group.add( mesh );
 
+      model.mesh = mesh;
+
+      console.log(model);
+
+      mesh.visible = model.visible;
+
+      if (mesh.visible)
+      {
+
+        targetObject = mesh;
+
+        WebMR.start(renderer, camera, scene, container, animate, WebMR.CAMERA_MODE_DEVICECAM, targetObject);
+
+        objectControl = new THREE.TrackObjectControls(mesh, camera, renderer.domElement);
+
+      }
+
     }
 
     var mmdModels = {
-     /* cafe_chan: {
-        modelFile: 'models/cafe_mmd/cafe_chan.pmd'
-      },
-      tea_chan: {
-        modelFile: 'models/tea_mmd/tea_chan.pmd',
-      } ,
+     /*
       shachiku_chan: {
         modelFile: 'models/shachiku_mmd/shachiku_chan.pmd',
+      },
+       cafe_chan: {
+        modelFile: 'models/cafe_mmd/cafe_chan.pmd',
+        imageUrl: 'http://cafechan.net/images/sec02thumb1off.png'
       },*/
+      tea_chan: {
+        modelFile: 'models/tea_mmd/tea_chan.pmd',
+        imageUrl: 'http://cafechan.net/images/sec02thumb2off.png',
+        visible: true
+      } ,
+
       kizunaai : {
-        modelFile: 'models/kizunaai_mmd/kizunaai.pmx'
+        modelFile: 'models/kizunaai_mmd/kizunaai.pmx',
+        imageUrl: 'https://kizunaai.com/acin/project/wp-content/themes/shapely-child/img/AI.png',
+        visible: false
       }
     }
 
@@ -101,17 +238,35 @@ $(function() {
     var loader = new THREE.MMDLoader();
 
     for (var id in mmdModels){
+
       var mmdModel = mmdModels[id];
 
-      if(mmdModel.vmdFiles && mmdMode.vmdFiles.length > 0) {
+      (function(mmdModel){
 
-        loader.load( mmdModel.modelFile, mmdModel.vmdFiles ,onLoad, onProgress, onError );
+        if(mmdModel.vmdFiles && mmdModel.vmdFiles.length > 0) {
 
-      } else {
 
-        loader.loadModel( mmdModel.modelFile, onLoad, onProgress, onError );
+          loader.load( mmdModel.modelFile, mmdModel.vmdFiles ,function(mesh){
 
-      }
+            onLoad(mesh, mmdModel);
+
+          }, onProgress, onError );
+
+
+        } else {
+
+          loader.loadModel( mmdModel.modelFile, function(mesh){
+            onLoad(mesh, mmdModel);
+          }, onProgress, onError );
+
+        }
+
+        if(mmdModel.imageUrl){
+
+          addCharacterSelectDialog(mmdModel);
+
+        }
+      })(mmdModel);
     }
 
     function makePhongMaterials ( mesh ) {
@@ -136,9 +291,6 @@ $(function() {
       return array;
 
     }
-
-
-
 
     var geometries = [
       new THREE.BoxGeometry( 0.2, 0.2, 0.2 ),
@@ -170,7 +322,6 @@ $(function() {
       group.add( object );
     }
 
-    WebMR.start(renderer, camera, scene, container, animate, WebMR.CAMERA_MODE_DEVICECAM);
     //WebMR.start(renderer, camera, scene, container, animate, WebMR.CAMERA_MODE_ORIENTATION);
   }
 
@@ -178,7 +329,13 @@ $(function() {
   * animate function.
   */
   function animate(){
+    if(objectControl){
 
+      //objectControl.update();
+
+    }
   }
+
+  initScene(true);
 
 });
--- app/scripts/webmr.js
+++ app/scripts/webmr.js
@@ -14,6 +14,9 @@ var WebMR = (function(){
     videoImageContext, videoTexture, cameraPlane,
     videoWidth, videoHeight;
 
+  var controls;
+
+
   let WebMR = {};
 
   // camera control mode
@@ -71,13 +74,33 @@ var WebMR = (function(){
       WebMR.getWebcamTexture()
         .then(function(tex){
 
+        console.log("loaded webcam texture");
         console.log(size * videoHeight / videoWidth);
 
         let geometry = new THREE.PlaneGeometry(size, size * videoHeight / videoWidth);
 
         let mesh = new THREE.Mesh(geometry, tex);
 
-        mesh.position.set(0, 0, -11);
+        var forward = new THREE.Vector3(0, 0, - 1.0);
+
+        forward.applyQuaternion(camera.quaternion);
+
+        forward.multiplyScalar(20.0);
+
+        forward.add(camera.position)
+
+        //var q = new THREE.Quaternion();
+
+        //q.setFromEuler(new THREE.Euler(0, Math.PI, 0 ,'YXZ'));
+
+        //mesh.applyQuaternion(q);
+
+        mesh.position.copy(forward);
+
+        mesh.applyQuaternion(camera.quaternion);
+
+        console.log(camera.position);
+        console.log(mesh.position);
 
         camera.add(mesh);
 
@@ -91,23 +114,35 @@ var WebMR = (function(){
 
   }
 
-  WebMR.start = function(renderer, camera, scene, container, animate, cameraMode){
+  WebMR.resizeCameraPlane = function(size, camera) {
+
+    cameraPlane.scale.x = size;
+    cameraPlane.scale.y = size;
+
+  }
+
+  WebMR.start = function(renderer, camera, scene, container, animate, cameraMode, object){
 
     window.addEventListener('resize', resize, false);
 
     WebVRSetting.init(renderer, camera, scene, "./../images/controllers/", {
-      useStereoVR: true,
+      useStereoVR: false,
       useHeadsetVR: false
     });
 
+    if(object == null){
 
-    var controls;
+      object = camera;
+
+    }
 
     if( cameraMode === WebMR.CAMERA_MODE_DEVICECAM) {
 
       WebMR.createCameraPlane(10, camera).then(function(){
 
-        controls = new WebMR.DeviceCameraControls(camera, video, videoImage);
+        //controls = new WebMR.DeviceCameraControls(camera, video, videoImage);
+
+        controls = new WebMR.DeviceCameraOrientationControls (camera, video, videoImage);
 
       });
 
@@ -136,7 +171,7 @@ var WebMR = (function(){
         controls.connect();
         controls.update();
         renderer.domElement.addEventListener('click', () => {
-          setFullScreen();
+          WebMR.setFullScreen(container);
         }, false);
 
         window.removeEventListener('deviceorientation',
@@ -152,8 +187,10 @@ var WebMR = (function(){
     WebVRSetting.startLoop(scene, camera, function(){
 
       if(cameraMode === WebMR.CAMERA_MODE_ORIENTATION){
+
         videoImageContext.drawImage(video, 0, 0,
           videoImage.width, videoImage.height);
+
       }
 
 
@@ -182,94 +219,165 @@ var WebMR = (function(){
       camera.updateProjectionMatrix();
 
       renderer.setSize(width, height);
-      camera.remove(cameraPlane);
-      WebMR.createCameraPlane(15, camera);
+      //camera.remove(cameraPlane);
+      //WebMR.createCameraPlane(15, camera);
+      WebMR.resizeCameraPlane(width/ 1000.0, camera);
       WebVRSetting.effect.setSize(width, height);
-    }
-
-    let nop = function(){};
-
-    /* screen 系*/
-    function setFullScreen(startFunc, endFunc,
-      failedFunc, lockMode = "landscape"){
-
-      startFunc = startFunc ? startFunc : nop;
-      endFunc = endFunc ? endFunc : nop;
-      failedFunc = failedFunc ? failedFunc : nop;
-
-      container.requestFullscreen = container.requestFullscreen ||
-        container.mozRequestFullScreen ||
-        container.webkitRequestFullScreen ||
-        container.webkitRequestFullscreen ||
-        container.msRequestFullscreen;
-
-      let fullScreenChangeList = [
-        "fullscreenchange",
-        "mozfullscreenchange",
-        "webkitfullscreenchange",
-        "MSFullscreenchange"
-      ];
-
-      document.fullscreenEnabled = document.fullscreenEnabled ||
-        document.mozFullScreenEnabled ||
-        document.webkitFullscreenEnabled ||
-        document.msFullscreenEnabled;
-
-      let fullScreenErrorList = [
-        "fullscreenerror",
-        "webkitfullscreenerror",
-        "mozfullscreenerror",
-        "MSFullscreenError"
-      ];
-
-      container.requestFullscreen();
-
-      let changedFuc = function( event ) {
-        if (document.fullscreenEnabled) {
-          if (lockMode)
-            lockOrientation(lockMode);
-          startFunc();
-        } else
-          endFunc();
-      };
-
-      let errFunc = function(event){
-        failedFunc();
-      };
-
-      for (let index in fullScreenChangeList){
-        container.addEventListener(fullScreenChangeList[index], changedFuc);
-
-        container.addEventListener(fullScreenErrorList[index], errFunc);
-      }
-      // setTimeout(resize, 2000);
 
     }
 
-    function lockOrientation(mode) {
-      console.log("lockOrientation");
-      if (screen.orientation.lock) {
-        screen.orientation.lock(mode).catch(function(e) {
-          console.log(e);
-        });
-      } else if (screen.lockOrientation) {
-        screen.lockOrientation(mode).catch(function(e) {
-          console.log(e);
-        });
-      } else if (screen.webkitLockOrientation) {
-        screen.webkitLockOrientation(mode).catch(function(e) {
-          console.log(e);
-        });
-      } else if (screen.mozLockOrientation) {
-        screen.mozLockOrientation(mode).catch(function(e) {
-          console.log(e);
-        });
-      } else if (screen.msLockOrientation) {
-        screen.msLockOrientation(mode).catch(function(e) {
-          console.log(e);
-        });
-      }
+  }
+
+  let nop = function(){};
+
+  /* screen 系*/
+  WebMR.setFullScreen = function(container, startFunc, endFunc,
+    failedFunc, lockMode = "landscape"){
+
+    startFunc = startFunc ? startFunc : nop;
+    endFunc = endFunc ? endFunc : nop;
+    failedFunc = failedFunc ? failedFunc : nop;
+
+    container.requestFullscreen = container.requestFullscreen ||
+      container.mozRequestFullScreen ||
+      container.webkitRequestFullScreen ||
+      container.webkitRequestFullscreen ||
+      container.msRequestFullscreen;
+
+    let fullScreenChangeList = [
+      "fullscreenchange",
+      "mozfullscreenchange",
+      "webkitfullscreenchange",
+      "MSFullscreenchange"
+    ];
+
+    document.fullscreenEnabled = document.fullscreenEnabled ||
+      document.mozFullScreenEnabled ||
+      document.webkitFullscreenEnabled ||
+      document.msFullscreenEnabled;
+
+    let fullScreenErrorList = [
+      "fullscreenerror",
+      "webkitfullscreenerror",
+      "mozfullscreenerror",
+      "MSFullscreenError"
+    ];
+
+    container.requestFullscreen();
+
+    let changedFuc = function( event ) {
+      if (document.fullscreenEnabled) {
+        if (lockMode)
+          WebMR.lockOrientation(lockMode);
+        startFunc();
+      } else
+        endFunc();
+    };
+
+    let errFunc = function(event){
+      failedFunc();
+    };
+
+    for (let index in fullScreenChangeList){
+      container.addEventListener(fullScreenChangeList[index], changedFuc);
+
+      container.addEventListener(fullScreenErrorList[index], errFunc);
     }
+    // setTimeout(resize, 2000);
+
+  }
+
+  WebMR.lockOrientation = function(mode) {
+    console.log("lockOrientation");
+    if (screen.orientation.lock) {
+      screen.orientation.lock(mode).catch(function(e) {
+        console.log(e);
+      });
+    } else if (screen.lockOrientation) {
+      screen.lockOrientation(mode).catch(function(e) {
+        console.log(e);
+      });
+    } else if (screen.webkitLockOrientation) {
+      screen.webkitLockOrientation(mode).catch(function(e) {
+        console.log(e);
+      });
+    } else if (screen.mozLockOrientation) {
+      screen.mozLockOrientation(mode).catch(function(e) {
+        console.log(e);
+      });
+    } else if (screen.msLockOrientation) {
+      screen.msLockOrientation(mode).catch(function(e) {
+        console.log(e);
+      });
+    }
+  }
+
+  /*
+    movement is controlled by device camera
+    ratation is controlled by device orientation
+  */
+
+  WebMR.DeviceCameraOrientationControls = function(object, video, canvas){
+
+    this.object = object;
+
+    this.deviceCameraConrols = new WebMR.DeviceCameraControls(object, video, canvas);
+
+    this.deviceOrientationControls = new WebMR.DeviceOrientationControls(object);
+
+    this.deviceCameraConrols.connect();
+
+    this.deviceOrientationControls.connect();
+
+    var target = new THREE.Vector3();
+
+    var forward = new THREE.Vector3(0, 0, 1.0);
+
+    this.update = function(){
+
+      var moveVec3 = this.deviceCameraConrols.update();
+
+      target.add(moveVec3);
+
+      this.deviceOrientationControls.update();
+
+      var distance = target.clone()
+        .sub(this.object.position)
+        .length();
+
+      var distVec = forward.clone()
+        .applyQuaternion(this.object.quaternion)
+        .multiplyScalar(distance)
+        .add(target);
+
+      this.object.position.set(distVec.x, distVec.y, distVec.z);
+
+    }
+
+    this.connect = function(){
+
+      this.deviceCameraConrols.connect();
+
+      this.deviceOrientationControls.connect();
+
+    }
+
+    this.disconnect = function(){
+
+      this.deviceCameraConrols.disconnect();
+
+      this.deviceOrientationControls.disconnect();
+
+    }
+
+    this.reset = function(_target){
+
+      target = _target || new THREE.Vector3();
+
+      this.deviceCameraConrols.reset();
+
+    }
+
   }
 
   /*
@@ -296,31 +404,9 @@ var WebMR = (function(){
     this.alpha = 0;
     this.alphaOffsetAngle = 0;
 
-    this.screenFlowControl = new  screen_flow_control(video, 2, canvas);
+    this.screenFlowControl = new screen_flow_control(video, 2, canvas);
 
-    var setObjectQuaternion = function() {
-
-      var zee = new THREE.Vector3( 0, 0, 1 );
-
-      var euler = new THREE.Euler();
-
-      var q0 = new THREE.Quaternion();
-
-      var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
-
-      return function( quaternion, alpha, beta, gamma, orient ) {
-
-        euler.set( beta, alpha, - gamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
-
-        quaternion.setFromEuler( euler ); // orient the device
-
-        quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
-
-        quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) ); // adjust for screen orientation
-
-      }
-
-    }();
+    var oldOffset = new THREE.Vector3();
 
     this.connect = function() {
 
@@ -334,27 +420,169 @@ var WebMR = (function(){
 
     };
 
+
+    this.update = function() {
+
+      return function(){
+
+        if ( scope.enabled === false ) return;
+
+        this.screenFlowControl.update();
+
+        var flow = this.screenFlowControl.get_data();
+
+        var offset = flow.move;
+
+        var rot = flow.rot;
+
+        var scale = 0.01;
+
+        var x = - (offset.x - oldOffset.x) * scale;
+
+        var y = (offset.y - oldOffset.y) * scale;
+
+        var z = - (offset.z - oldOffset.z) * scale;
+
+        var v = new THREE.Vector3(x, y, z);
+
+        v.applyQuaternion(scope.object.quaternion);
+
+        scope.object.position.add(v);
+
+        oldOffset = offset;
+
+        return v;
+
+      };
+
+    }();
+
+    this.dispose = function() {
+
+      this.disconnect();
+
+    };
+
+    this.reset = function(){
+
+      oldOffset = new THREE.Vector3();
+
+      this.screenFlowControl.reset();
+
+    }
+
+    this.connect();
+
+  }
+
+  /**
+ * @author richt / http://richt.me
+ * @author WestLangley / http://github.com/WestLangley
+ *
+ * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
+ */
+  WebMR.DeviceOrientationControls = function(object){
+
+    var scope = this;
+
+    var orientationChanaged = false;
+
+    this.object = object;
+    this.object.rotation.reorder( "YXZ" );
+
+    this.enabled = true;
+
+    this.deviceOrientation = {};
+    this.screenOrientation = 0;
+
+    this.alpha = 0;
+    this.alphaOffsetAngle = 0;
+
+
+    var onDeviceOrientationChangeEvent = function( event ) {
+
+      scope.deviceOrientation = event;
+
+      if(!orientationChanaged){
+
+        orientationChanaged = true;
+
+        //scope.alphaOffsetAngle = - THREE.Math.degToRad( scope.deviceOrientation.alpha);
+
+      }
+
+    };
+
+    var onScreenOrientationChangeEvent = function() {
+
+      scope.screenOrientation = window.orientation || 0;
+
+    };
+
+    // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
+
+    var setObjectQuaternion = function() {
+
+      var oldAlpha = 0.0, oldBeta = Math.PI/ 2.0, oldGamma = 0.0;
+
+      var zee = new THREE.Vector3( 0, 0, 1 );
+
+      var euler = new THREE.Euler();
+
+      var q0 = new THREE.Quaternion();
+
+      var q1 = new THREE.Quaternion();
+      q1.setFromEuler(new THREE.Euler(- Math.PI/2.0, 0, 0 ,'YXZ'));
+      //var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
+
+      return function( quaternion, alpha, beta, gamma, orient ) {
+/*
+        euler.set(  beta - oldBeta,  alpha - oldAlpha, - gamma + oldGamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
+        //euler.set(  - beta + oldBeta,  - alpha + oldAlpha,  gamma - oldGamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
+
+        quaternion.multiply( q0.setFromEuler(euler) ); // camera looks out the back of the device, not the top
+*/
+        euler.set(  beta,  alpha , - gamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
+
+        quaternion.setFromEuler( euler ); // orient the device
+
+        quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
+
+        quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) ); // adjust for screen orientation
+
+        oldAlpha = alpha;
+
+        oldBeta = beta;
+
+        oldGamma = gamma;
+      }
+
+    }();
+
+    this.connect = function() {
+
+      onScreenOrientationChangeEvent(); // run once on load
+
+      window.addEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+      window.addEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+
+      scope.enabled = true;
+
+    };
+
+    this.disconnect = function() {
+
+      window.removeEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+      window.removeEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+
+      scope.enabled = false;
+
+    };
+
     this.update = function() {
 
       if ( scope.enabled === false ) return;
 
-      this.screenFlowControl.update();
-
-      var flow = this.screenFlowControl.get_data();
-
-      var offset = flow.move;
-      var rot = flow.rot;
-
-
-      var scale = 0.01;
-
-      var pos = new THREE.Vector3(-offset.x * scale, offset.y*scale, offset.z * scale);
-
-      pos.add(initPosition);
-
-      scope.object.position.set(pos.x, pos.y, pos.z);
-
-      /*
       var alpha = scope.deviceOrientation.alpha ? THREE.Math.degToRad( scope.deviceOrientation.alpha ) + this.alphaOffsetAngle : 0; // Z
       var beta = scope.deviceOrientation.beta ? THREE.Math.degToRad( scope.deviceOrientation.beta ) : 0; // X'
       var gamma = scope.deviceOrientation.gamma ? THREE.Math.degToRad( scope.deviceOrientation.gamma ) : 0; // Y''
@@ -362,7 +590,14 @@ var WebMR = (function(){
 
       setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
       this.alpha = alpha;
-    */
+
+    };
+
+    this.updateAlphaOffsetAngle = function( angle ) {
+
+      this.alphaOffsetAngle = angle;
+      this.update();
+
     };
 
     this.dispose = function() {
@@ -372,6 +607,17 @@ var WebMR = (function(){
     };
 
     this.connect();
+
+  }
+
+  WebMR.resetControls = function()
+  {
+
+    if(controls){
+
+      controls.reset();
+
+    }
 
   }
 
